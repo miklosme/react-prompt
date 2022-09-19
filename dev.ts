e3759707544b6d9fccb203292afc53dd76d53593
Miklos Megyes <m.miklos05@gmail.com>
2022-09-19 17:38:45 +0200

    add inquirer

---
+++ dev.ts
@@ -0,0 +1,43 @@
+import inquirer from 'inquirer';
+// import { promises as fs } from 'fs';
+import path from 'path';
+import { globby } from 'globby';
+
+const paths = await globby(['examples/**/*.{ts,tsx,js,jsx}', '!**/resolvers']);
+
+const options = paths
+  .map((p) => ({
+    value: p,
+    name: path.basename(p),
+    dir: path.dirname(p).replace(/^examples\//, ''),
+  }))
+  .reduce((acc, { value, name, dir }) => {
+    if (!acc[dir]) {
+      acc[dir] = [];
+    }
+    acc[dir].push({ value, name });
+    return acc;
+  }, {});
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'example',
+      message: 'Which example do you want to run?',
+      choices: Object.entries(options).flatMap(([dir, choices]) => [new inquirer.Separator(dir), ...choices]),
+    },
+  ])
+  .then((answers) => {
+    console.log(answers.example);
+    process.exit(0);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+
+    throw error;
+  });
